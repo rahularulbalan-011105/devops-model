d826efa2dff4fe23d2e12b81863383c52754258b
A Rahul <23d101@psgitech.ac.in>
2026-04-14 22:06:30 +0530

    Add files via upload

---
+++ stream.py
@@ -0,0 +1,16 @@
+# app.py
+import streamlit as st
+import datetime
+
+st.title("🚀 My Simple CD Streamlit App")
+
+st.write("If this updates after Git push, your pipeline works 😌")
+
+# dynamic content to see refresh
+st.write("Current time:")
+st.success(datetime.datetime.now())
+
+name = st.text_input("Enter your name:")
+
+if name:
+    st.write(f"Hello {name}, Jenkins CD is working!")
